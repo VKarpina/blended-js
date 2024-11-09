7929f7ecb8475868c9af08a7453def607c182b2b
VKarpina <v.karpina01@gmail.com>
2024-11-09 11:42:40 +0100

    final

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -4,11 +4,14 @@ class UnsplashAPI {
 	#BASE_URL = 'https://api.unsplash.com/search/photos';
 	#API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
 	#query = '';
+	#params = new URLSearchParams({
+		orientation: 'portrait',
+		client_id: this.#API_KEY,
+		per_page: 12,
+	});
 
 	async fetchPopularPhotos(page) {
-		const url = `${
-			this.#BASE_URL
-		}?query=popular&page=${page}&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`;
+		const url = `${this.#BASE_URL}?query=popular&page=${page}&${this.#params}`;
 
 		try {
 			const { data } = await axios(url);
@@ -18,9 +21,7 @@ class UnsplashAPI {
 		}
 	}
 	async fetchPhotosByQuery(page) {
-		const url = `${this.#BASE_URL}?query=${
-			this.#query
-		}&page=${page}&per_page=12&orientation=portrait&client_id=${this.#API_KEY}`;
+		const url = `${this.#BASE_URL}?query=${this.#query}&page=${page}&${this.#params}`;
 
 		try {
 			const { data } = await axios(url);
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,10 +2,13 @@ import UnsplashAPI from './UnsplashAPI';
 import renderFunction from './renderFunction';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const api = new UnsplashAPI();
 const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
+const loader = document.querySelector('.loader');
 
 const container = document.getElementById('tui-pagination-container');
 const pagination = new Pagination(container, {
@@ -23,12 +26,7 @@ api.fetchPopularPhotos(page).then(data => {
 	renderFunction(data.results, gallery);
 });
 
-pagination.on('afterMove', event => {
-	const currentPage = event.page;
-	api.fetchPopularPhotos(currentPage).then(data => {
-		renderFunction(data.results, gallery);
-	});
-});
+pagination.on('afterMove', getPopular);
 
 form.addEventListener('submit', event => {
 	event.preventDefault();
@@ -36,9 +34,58 @@ form.addEventListener('submit', event => {
 	if (value === '') {
 		return;
 	}
+	pagination.off('afterMove', getPopular);
+	pagination.off('afterMove', getByQuery);
+	gallery.innerHTML = '';
 	api.query = value;
-	api.fetchPhotosByQuery(page).then(data => {
-		renderFunction(data.results, gallery);
-		pagination.reset(data.total);
-	});
+	showElement(loader);
+	api
+		.fetchPhotosByQuery(page)
+		.then(data => {
+			if (data.results.length === 0) {
+				iziToast.info({ message: 'Photos not found, try another query' });
+				hideElement(container);
+				return;
+			}
+			renderFunction(data.results, gallery);
+			pagination.reset(data.total);
+			if (data.total <= 12) {
+				hideElement(container);
+			} else {
+				showElement(container);
+			}
+			iziToast.success({ message: `We found ${data.total} photos` });
+		})
+		.catch(error => {
+			console.log(error);
+			iziToast.error({ message: 'Something went wrong' });
+		})
+		.finally(() => {
+			hideElement(loader);
+			form.reset();
+		});
+
+	pagination.on('afterMove', getByQuery);
 });
+
+function getPopular(event) {
+	const currentPage = event.page;
+	api.fetchPopularPhotos(currentPage).then(data => {
+		renderFunction(data.results, gallery);
+	});
+}
+
+function getByQuery(event) {
+	const currentPage = event.page;
+	api.fetchPhotosByQuery(currentPage).then(data => {
+		renderFunction(data.results, gallery);
+	});
+}
+
+function showElement(element) {
+	element.classList.remove('hidden');
+}
+
+function hideElement(element) {
+	element.classList.add('hidden');
+}
