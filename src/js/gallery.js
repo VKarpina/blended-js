051aa9fd7a25d27247ce5525aee30e04da23aea8
Eduard <parhomenkoeduard75@gmail.com>
2024-11-09 11:09:39 +0200

    feat: render, pagination

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,7 +1,23 @@
 import UnsplashAPI from './UnsplashAPI';
+import renderFuntion from './renderFunction';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
 
 const api = new UnsplashAPI();
+const gallery = document.querySelector('.gallery');
 
-api.fetchPopularPhotos(1).then(data => {
+const container = document.getElementById('tui-pagination-container');
+const pagination = new Pagination(container, {
+	totalItems: 0,
+	itemsPerPage: 12,
+	visiblePages: 5,
+	page: 1,
+});
+
+const page = pagination.getCurrentPage();
+
+api.fetchPopularPhotos(page).then(data => {
 	console.log(data);
+	pagination.reset(data.total);
+	renderFuntion(data.results, gallery);
 });
